6f0e19fa60988712ddbbc9a6e30febc34f153e48
German Cabarcas <gcc@unironical.com>
2023-03-07 05:04:52 -0500

    Added album switching interactions and active states

--- assets/ts/works_player.ts
+++ assets/ts/works_player.ts
@@ -15,7 +15,7 @@ const albumData = JSON.parse(document.getElementById('albumData').textContent);
 let selectedAlbumIndex = 0;
 
 function init() {
-  const albumImages = document.querySelectorAll('.wop-Album img');worli
+  const albumImages = document.querySelectorAll('.wop-Album img');
   
   // Add click event listeners to album images
   albumImages.forEach((img) => {
@@ -44,6 +44,28 @@ function selectAlbum(albumIndex: number) {
   const albumDescription = document.getElementById('album-description');
   albumDescription.innerHTML = albumData.albums[selectedAlbumIndex].description;
   
+  const albumCovers = albumData.albums[selectedAlbumIndex]?.images.map(image => {
+    const srcset = image.srcset.join('');
+    const pic = document.createElement('figure');
+    const _source = document.createElement('source');
+    const img = document.createElement('img');
+
+    _source.setAttribute('srcset', srcset);
+    pic.appendChild(_source);
+    img.setAttribute('src', image.src);
+    img.classList.add('wop-CurrentCovers_Image');
+    pic.appendChild(img);
+    return pic;
+  });
+
+  const activeAlbum = document.querySelector('.wop-AlbumList .active');
+  activeAlbum?.classList.remove('active');
+  const albums = document.querySelectorAll('.wop-Album');
+  albums[albumIndex].classList.add('active');
+  const currentCovers = document.querySelector('.wop-CurrentCovers');
+  currentCovers.innerHTML = '';
+  albumCovers.forEach(cover => currentCovers.appendChild(cover));
+
   // Set track list
   const trackList = document.getElementById('track-list');
   const tracks = albumData.albums[selectedAlbumIndex].tracks;
@@ -54,16 +76,20 @@ function selectAlbum(albumIndex: number) {
     trackSpan.addEventListener('click', () => {
       selectTrack(index);
     });
+    index === 0 && trackSpan.classList.add("active");
     trackList.appendChild(trackSpan);
   });
 }
 
 function selectTrack(trackIndex: number) {
   // Set track source and play
+  const activeTrack = document.querySelector('#track-list .active')
+  activeTrack?.classList.remove('active');
   const trackSrc = albumData.albums[selectedAlbumIndex].tracks[trackIndex].src;
   audio.src = trackSrc;
   audio.load();
   audio.play();
+  document.querySelectorAll('#track-list span')[trackIndex].classList.add('active');
 }
 
 init();
